620aaf644aadada0dea50e3880b1a3d985ecb9ec
lxfgit <lxf_alantic@163.com>
2016-07-25 18:53:06 +0800

    2016-7-25 18:52:55

--- src/linkedlist/common/Node.java
+++ src/linkedlist/common/Node.java
@@ -1,12 +1,14 @@
 package linkedlist.common;
 
 /**
+ * 链表的结构
  * Created by LXF on 2016/7/25.
  */
 public class Node {
     public int value;
     public Node next;
 
+    /*为添加一个节点的方法*/
     public Node(int data) {
         this.value = data;
     }
